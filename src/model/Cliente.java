106835a27a1866d3d78dd98be472297448ba29f6
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-11-10 10:20:09 -0300

    Classe cliente criada

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,43 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+	private String cpf;
+	private String telefone;
+	
+	public Cliente(String nome, String email, String cpf, String telefone) {
+		this.nome = nome;
+		this.email = email;
+		this.cpf = cpf;
+		this.telefone = telefone;
+	}
+	
+	public Cliente() {}
+	
+	public Long getId() { return id; }
+	public void setId(Long id) { this.id = id; }
+	
+	public String getNome() { return nome; }
+	public void setNome(String nome) { this.nome = nome; }
+	
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
+	
+	public String getCpf() { return cpf; }
+	public void setCpf(String cpf) { this.cpf = cpf; }
+	
+	public String getTelefone() { return telefone; }
+	public void setTelefone(String telefone) { this.telefone = telefone; }
+	
 }
